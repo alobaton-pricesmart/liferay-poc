db18354874ad93b3004cb34479a36493dc806e0b
Alvaro Jose Lobaton Restrepo <alobaton@itcol.com>
2019-04-23 11:50:50 -0500

    Initial commit

---
+++ src/main/java/com/co/liferaypoc/constants/LiferayPocPortletKeys.java
@@ -0,0 +1,10 @@
+package com.co.liferaypoc.constants;
+
+/**
+ * @author alobaton
+ */
+public class LiferayPocPortletKeys {
+
+	public static final String LiferayPoc = "liferaypoc";
+
+}
---
+++ src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,6 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<h1>Hello World!</h1>
+	<b><liferay-ui:message key="liferaypoc.caption"/></b>
+</p>
